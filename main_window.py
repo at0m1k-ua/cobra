6710671602b55e2dc9c000663ce57006221e3d14
JPlay42 <kuruch2004@gmail.com>
2022-12-28 00:09:38 +0200

    inherit MainWindow from QMainWindow

--- main_window.py
+++ main_window.py
@@ -1,6 +1,8 @@
+from PyQt6.uic.uiparser import QtWidgets
+
 from main_window_ui import MainWindowUI
 
 
-class MainWindow:
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         self.__ui = MainWindowUI()
