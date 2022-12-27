d446884d31c854a833f5336a73e470509ccfae3b
clientforgit <clientforgit@gmail.com>
2022-12-27 23:27:22 +0200

    add example in custom_main_window_ui.py

--- custom_main_window_ui.py
+++ custom_main_window_ui.py
@@ -1,3 +1,5 @@
+from PyQt6 import QtWidgets
+
 from main_window_ui import Ui_mainWindow
 
 class CustomMainWindow(Ui_mainWindow):
@@ -5,3 +7,12 @@ class CustomMainWindow(Ui_mainWindow):
         super().setupUi(mainWindow)
         mainWindow.setFixedHeight(352)
         mainWindow.setFixedWidth(202)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = QtWidgets.QMainWindow()
+    ui = CustomMainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec())
