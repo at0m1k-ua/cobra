3e9e2a86ffc2c3803277673edc2b666c00c45a93
JPlay42 <kuruch2004@gmail.com>
2022-12-28 00:13:43 +0200

    create class AudioPlayer with method signatures

---
+++ audio_player.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+class AudioPlayer:
+
+    def play(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def prev(self):
+        pass
+
+    def next(self):
+        pass
+
+    def add(self, path: Path):
+        pass
+
+    def remove(self, index: int):
+        pass
